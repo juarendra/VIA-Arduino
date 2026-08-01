a03f5fb500b570c15d337b4c29ac525a215729de
Juarendra Ramadhani <jrjuarendra@gmail.com>
2026-08-01 23:26:16 +0700

    test(matrix): add failing ROW2COL scan test

--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -10,10 +10,7 @@ class FakeMatrixIO : public via::MatrixIO {
     for (uint8_t i = 0; i < sizeof(pinStates); ++i) pinStates[i] = true;
   }
 
-  void inputPullup(via::Pin pin) override {
-    ++inputPullupCalls;
-    pinStates[pin & 0x1F] = true;
-  }
+  void inputPullup(via::Pin) override { ++inputPullupCalls; }
   void driveLow(via::Pin pin) override {
     ++driveLowCalls;
     pinStates[pin & 0x1F] = false;
@@ -72,6 +69,24 @@ int main() {
     assert(scanMatrix.begin());
     scanMatrix.task(0);
     assert(scanMatrix.rawRow(0) == 0x01);
+  }
+
+  // ROW2COL: drive columns low, read rows
+  {
+    via::Pin rowPins[2] = {100, 101};
+    via::Pin colPins[3] = {200, 201, 202};
+    uint32_t rawRows[2] = {0, 0};
+    uint32_t candidate[2] = {0, 0};
+    uint32_t stable[2] = {0, 0};
+    uint32_t changed[2] = {0, 0};
+    FakeMatrixIO io2;
+    io2.pinStates[100 & 0x1F] = false;
+    via::MatrixConfig cfg2 = {2, 3, rowPins, colPins, via::kRowToCol, 30, 5,
+                              rawRows, candidate, stable, changed};
+    via::Matrix matrix2(cfg2, io2);
+    assert(matrix2.begin());
+    matrix2.task(0);
+    assert(matrix2.rawRow(0) != 0);
   }
 
   return 0;

0e497430693a5112b08702b452c8e70f5f67f5c0
Juarendra Ramadhani <jrjuarendra@gmail.com>
2026-08-01 19:27:03 +0700

    test(tinyusb): define device lifetime contract

--- tests/fakes/Adafruit_TinyUSB.h
+++ tests/fakes/Adafruit_TinyUSB.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -10,18 +11,48 @@ static const hid_report_type_t HID_REPORT_TYPE_OUTPUT = 2;
 
 class Adafruit_USBD_HID {
  public:
-  Adafruit_USBD_HID(const uint8_t*, size_t, uint8_t, uint8_t, bool) {}
+  typedef void (*SetReportCallback)(uint8_t, hid_report_type_t, const uint8_t*,
+                                    uint16_t);
+
+  Adafruit_USBD_HID(const uint8_t*, size_t, uint8_t, uint8_t, bool)
+      : setReportCallback_(0), alive_(true) {}
+  ~Adafruit_USBD_HID() { alive_ = false; }
 
   void enableOutEndpoint(bool) {}
   void setPollInterval(uint8_t) {}
   void setStringDescriptor(const char*) {}
-  void setReportCallback(
-      void*, void (*)(uint8_t, hid_report_type_t, const uint8_t*, uint16_t)) {}
-  bool begin() { return beginResult; }
-  bool ready() { return true; }
-  bool sendReport(uint8_t, const uint8_t*, size_t) { return true; }
+  void setReportCallback(void*, SetReportCallback callback) {
+    setReportCallback_ = callback;
+  }
+  bool begin() {
+    ++beginAttempts;
+    if (registeredInterface) return false;
+    registeredInterface = this;
+    return beginResult;
+  }
+  bool ready() { return readyResult; }
+  bool sendReport(uint8_t, const uint8_t*, size_t) { return sendResult; }
+
+  static void dispatchSetReport(uint8_t reportId,
+                                hid_report_type_t reportType,
+                                const uint8_t* buffer, uint16_t length) {
+    assert(registeredInterface);
+    assert(registeredInterface->alive_);
+    if (registeredInterface->setReportCallback_) {
+      registeredInterface->setReportCallback_(reportId, reportType, buffer,
+                                               length);
+    }
+  }
 
   static bool beginResult;
+  static bool readyResult;
+  static bool sendResult;
+  static size_t beginAttempts;
+  static Adafruit_USBD_HID* registeredInterface;
+
+ private:
+  SetReportCallback setReportCallback_;
+  bool alive_;
 };
 
 class TinyUSBDeviceClass {
--- tests/tinyusb_rawhid_test.cpp
+++ tests/tinyusb_rawhid_test.cpp
@@ -1,25 +1,90 @@
 #include <assert.h>
+#include <string.h>
 
 #include "VIA_TinyUSB_RawHID.h"
 
 bool Adafruit_USBD_HID::beginResult = true;
+bool Adafruit_USBD_HID::readyResult = true;
+bool Adafruit_USBD_HID::sendResult = true;
+size_t Adafruit_USBD_HID::beginAttempts = 0;
+Adafruit_USBD_HID* Adafruit_USBD_HID::registeredInterface = 0;
 TinyUSBDeviceClass TinyUSBDevice;
 
-int main() {
-  {
-    via::tinyusb::RawHID failed;
-    Adafruit_USBD_HID::beginResult = false;
-    assert(!failed.begin());
+static void assertInactive(via::tinyusb::RawHID& hid) {
+  uint8_t packet[via::kPacketSize] = {};
+  assert(!hid.receive(packet));
+  assert(!hid.send(packet));
+  assert(!hid.ready());
+  assert(!hid.sendComplete());
+}
 
-    via::tinyusb::RawHID replacement;
-    Adafruit_USBD_HID::beginResult = true;
-    assert(replacement.begin());
-  }
+static void assertFailedBeginIsFinal() {
+  Adafruit_USBD_HID::beginResult = false;
+  via::tinyusb::RawHID failed;
+  assert(!failed.begin());
+  assert(Adafruit_USBD_HID::beginAttempts == 1);
+  assertInactive(failed);
+
+  uint8_t report[via::kPacketSize] = {0x31};
+  Adafruit_USBD_HID::dispatchSetReport(0, HID_REPORT_TYPE_OUTPUT, report,
+                                      sizeof(report));
+  uint8_t received[via::kPacketSize] = {};
+  assert(!failed.receive(received));
+
+  Adafruit_USBD_HID::beginResult = true;
+  via::tinyusb::RawHID replacement;
+  assert(!replacement.begin());
+  assert(Adafruit_USBD_HID::beginAttempts == 1);
+  assertInactive(replacement);
+}
+
+static void assertSuccessfulOwnerIsFinal() {
+  uint8_t first[via::kPacketSize] = {0x41};
+  uint8_t second[via::kPacketSize] = {0x42};
 
   {
     via::tinyusb::RawHID owner;
     assert(owner.begin());
+    assert(Adafruit_USBD_HID::beginAttempts == 1);
+
+    via::tinyusb::RawHID rejected;
+    assert(!rejected.begin());
+    assert(Adafruit_USBD_HID::beginAttempts == 1);
+    assertInactive(rejected);
+
+    Adafruit_USBD_HID::dispatchSetReport(0, HID_REPORT_TYPE_OUTPUT, first,
+                                        sizeof(first));
+    Adafruit_USBD_HID::dispatchSetReport(0, HID_REPORT_TYPE_OUTPUT, second,
+                                        sizeof(second));
+    uint8_t received[via::kPacketSize] = {};
+    assert(owner.receive(received));
+    assert(memcmp(received, first, sizeof(first)) == 0);
+    assert(!owner.receive(received));
+
+    assert(owner.send(first));
+    Adafruit_USBD_HID::readyResult = false;
+    assert(!owner.send(first));
+    assert(!owner.ready());
+    assert(!owner.sendComplete());
+    Adafruit_USBD_HID::readyResult = true;
+    assert(owner.ready());
+    assert(owner.sendComplete());
   }
+
+  Adafruit_USBD_HID::dispatchSetReport(0, HID_REPORT_TYPE_OUTPUT, first,
+                                      sizeof(first));
   via::tinyusb::RawHID replacement;
-  assert(replacement.begin());
+  assert(!replacement.begin());
+  assert(Adafruit_USBD_HID::beginAttempts == 1);
+  assertInactive(replacement);
+}
+
+int main(int argc, char** argv) {
+  assert(argc == 2);
+  if (strcmp(argv[1], "failed") == 0) {
+    assertFailedBeginIsFinal();
+  } else {
+    assert(strcmp(argv[1], "successful") == 0);
+    assertSuccessfulOwnerIsFinal();
+  }
 }

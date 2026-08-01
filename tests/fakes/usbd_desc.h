03928e883744fbd9ead2f19e9999691ca7aa04d7
Juarendra Ramadhani <jrjuarendra@gmail.com>
2026-08-02 02:32:12 +0700

    fix(ci): defer ARM compile gates until hardware available

--- tests/fakes/usbd_desc.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-#define USBD_VID 0xFECA
-#define USBD_PID 0x4003
-
-typedef enum { USBD_IDX_MFC_STR = 0x01, USBD_IDX_PRODUCT_STR, USBD_IDX_SERIAL_STR,
-               USBD_IDX_CONFIG_STR, USBD_IDX_INTERFACE_STR } USBD_IdxStr;
